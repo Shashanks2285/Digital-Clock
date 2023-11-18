d13f074bb2050fd4daa31754c24a714e2d5642b6
Shashank Shekhar <shashankshekharlkrmit1@gmail.com>
2023-11-19 01:02:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App =()=>{
+  let time = new Date().toLocaleTimeString();
+  console.log(time);
+  const [ctime, uptime]= useState(time);
+
+  const updateT =()=>{
+    time = new Date().toLocaleTimeString();
+    uptime(time);
+  }
+
+  setInterval(updateT,999+1);
+  // automate
+
+
+  return(
+    <div>
+      <h1>{ctime}</h1> 
+      <h1 className="name">Designed By : Shashank Shekhar</h1>
+    </div>);
 }
 
 export default App;
